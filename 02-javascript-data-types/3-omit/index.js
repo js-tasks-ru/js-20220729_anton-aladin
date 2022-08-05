c6858d8261ec9ddc94b734e11bd41bab294f30c0
Anton Aladin <aladin.anton95@gmail.com>
2022-08-05 19:44:58 +0700

    3-omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  let constData = obj,
+    fieldsData = fields,
+    i = 0,
+    arrContainer = new Map(Object.entries(constData));
+  for (let key in constData) {
+    arrContainer.delete(fieldsData[i], constData[fieldsData[i]]);
+    i++;
+  }
+  return Object.fromEntries(arrContainer);
 };
