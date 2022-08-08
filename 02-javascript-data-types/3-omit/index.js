545ae5171f2f31dfe811684a4358647129b6530f
Anton Aladin <aladin.anton95@gmail.com>
2022-08-08 21:39:15 +0700

    fixed errors in 02-javascript-data-types

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,13 +5,15 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-  let constData = obj,
-    fieldsData = fields,
-    i = 0,
-    arrContainer = new Map(Object.entries(constData));
+  const constData = obj;
+  const fieldsData = fields;
+  let i = 0;
+  let arrContainer = new Map(Object.entries(constData));
   for (let key in constData) {
-    arrContainer.delete(fieldsData[i], constData[fieldsData[i]]);
-    i++;
+    if (Object.hasOwn(constData, fieldsData[i])) {
+      arrContainer.delete(fieldsData[i], constData[fieldsData[i]]);
+      i++;
+    }
   }
   return Object.fromEntries(arrContainer);
 };
